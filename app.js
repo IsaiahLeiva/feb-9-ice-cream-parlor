e284b01cf903757e0d4e4e11ef1078570e0f5536
Isaiah Leiva <isaiah.leiva@gmail.com>
2022-02-09 14:08:40 -0700

    commit

--- app.js
+++ app.js
@@ -4,3 +4,7 @@ const vessels = [{ name: 'Waffle Cone', image: 'https://m.media-amazon.com/image
 
 const toppings = [{ name: 'Sprinkles', image: 'https://upload.wikimedia.org/wikipedia/commons/f/f6/Sprinkles2.jpg', price: 1 }, { name: 'Chocolate Chips', image: 'https://www.eatthis.com/wp-content/uploads/sites/4/2020/05/chocolate-chips.jpg?quality=82&strip=1&resize=640%2C360', price: 2 }]
 
+//ANCHOR: When you select an ice cream or a vessel utilize the find method to add it to the cart array.
+
+//ANCHOR: Every time you add something to your cart the total should be updated, you can render this to the screen similar to what a receipt would look like.
+
